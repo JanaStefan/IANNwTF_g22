cca1d4252be200c13dc73a9a273ba3019a84888b
zandmahsa <104939235+zandmahsa@users.noreply.github.com>
2023-11-11 11:02:42 -1200

    Delete homework01/cat.py

--- homework01/cat.py
+++
@@ -1,6 +0,0 @@
-class cat:
-    def __init__(self, name):
-        self.name = name
-     
-    def say_smt(self, cat2):
-        print( f"Hello I am {self.name}! I see you are also a cool fluffy kitty Snowball {cat2.name}, let’s together purr at the human, so that they shall give us food.")
